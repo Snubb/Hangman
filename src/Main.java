7edb93c9d4f61c0d1ce951ed34ecd4a0cca8b431
Snubb <60389050+Snubb@users.noreply.github.com>
2021-01-15 11:06:27 +0100

    Kollar nu ifall ordet innehåller bokstaven som spelaren gissade på och ökar "gameover" ifall man gissar fel.

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,27 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) {
-        String word = randomWord();
+        String word = randomWord().toLowerCase();
         int gameOver = 0; //increases by one per wrong letter. Game over if 10.
         String visibleWord = "_".repeat(word.length());
+
         while (gameOver < 10) {
-            String guessWord = "Potat :)";
+            System.out.println(visibleWord);
+            String guessWord;
             while (true) {
-                guessWord = JOptionPane.showInputDialog("Guess a letter");
-                System.out.println(guessWord.length());
+                guessWord = JOptionPane.showInputDialog("Guess a letter").toLowerCase();
                 if (guessWord.length() > 1){
                     System.out.println("You can only guess a single letter.");
                 } else {
                     break;
                 }
             }
+            boolean containsLetter = word.contains(guessWord);
+            if (containsLetter) {
 
-
+            } else {
+                gameOver++;
+            }
         }
     }
 
