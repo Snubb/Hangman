5e77d8fd42d11890f6ca84cc119b027b32ae6035
Snubb <60389050+Snubb@users.noreply.github.com>
2021-01-15 10:29:41 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String word = randomWord();
+        int gameOver = 0; //increases by one per wrong letter. Game over if 10.
+    }
+
+    private static String randomWord() {
+
+
+        return null;
+    }
+
+
+}
