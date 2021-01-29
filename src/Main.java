8ab7dd5956aa0ac01d39935f416df5b8ebb8ce8e
Snubb <60389050+Snubb@users.noreply.github.com>
2021-01-29 10:55:19 +0100

    Spelaren kan nu se vilka ord hen har gissat på

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
         int gameOver = 0; //increases by one per wrong letter, game over if 9
         String visibleWord = "_".repeat(word.length()); //the visible word showed to the player
         char guessLetter;
+        StringBuilder guessedLetters = new StringBuilder(); //contains all the letters the player has already guessed
 
         //May look completely fucked but it's the hanging man I promise.
         String[] image = {"----------\n" + "----------\n" + "----------\n" + "----------\n" + "----------\n" + "--___-----\n" + "_/___\\____\n",
@@ -27,21 +28,25 @@ public class Main {
             if (visibleWord.equals(word)) {
                 break; //breaks out of loop if player has won
             }
-            System.out.println(image[gameOver] + " \n" + visibleWord + "\n"); //show the player their progress.
+            System.out.println(image[gameOver] + " \n" + visibleWord + "\nGuessed letters: " + guessedLetters + "\n"); //show the player their progress.
             String guessString = JOptionPane.showInputDialog("Guess a letter").toLowerCase();
             guessLetter = guessString.charAt(0);
-            if (word.contains(guessString)) {
-                visibleWord = newWord(word, guessLetter, visibleWord);
+            if (guessedLetters.toString().contains(guessString)) { //checks if the player has already guessed that letter.
+                System.out.println("You have already guessed that letter.");
+            } else if (word.contains(guessString)) { //checks if the word contains the guessed letter.
+                visibleWord = newWord(word, guessLetter, visibleWord); //
+                guessedLetters.append(guessLetter).append(" "); //adds the letter to the list of guessed letters.
             } else {
                 System.out.println("Try again");
                 gameOver++;
+                guessedLetters.append(guessLetter).append(" "); //adds the letter to the list of guessed letters.
             }
         }
         //checks if it's a win or a loss
         if (visibleWord.equals(word)) {
-            System.out.println("Congratulations! You have won.\nThe word was " + word);
+            System.out.println("Congratulations! You have won.\nThe word was " + word + ".");
         } else {
-            System.out.println(image[gameOver] + "\n\nYou killed him. This is your fault.\nThe word was " + word);
+            System.out.println(image[gameOver] + "\n\nYou killed him. This is your fault.\nThe word was " + word + ".");
         }
     }
 
